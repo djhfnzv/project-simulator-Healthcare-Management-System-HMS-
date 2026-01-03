48c5150e9af5da0692e118121e7f1d4f2d118ef0
djhfnzv <asifakber0003@gmail.com>
2026-01-03 18:42:13 +0600

    Feature 01 : User Registration (F02)
    -created Registration form
    -added input validation for each feild

--- PROJECT_FEATURES.md
+++ PROJECT_FEATURES.md
@@ -3,3 +3,10 @@ This task involves creating a login form component with email and passw
 ord fields, including validation and error handling.
 **Status: Implemented**
 
+## F02: User Registration 
+This task involves creating a login form component with email and passw
+ord fields, including validation and error handling.
+This task involves creating a registation form for patient, component with Name , age, Blood group, email and passw
+ord fields, including validation and error handling.
+**Status: Implemented**
+
---
+++ Registration/View/registration.php
@@ -0,0 +1,103 @@
+<?php
+    session_start();
+?>
+<html>
+    <head>
+        <title>Registration</title>
+        <link rel="stylesheet" href="../Asset/cssRegistration.css">
+    </head>
+    <body>
+        <script src="../Asset/scriptRegistration.js"></script>
+
+        <fieldset>
+            <h1>User Registration</h1>
+                <form action="../Controller/registrationCheck.php" method="post">
+                    <table>
+                        <tr>
+                            <td>Name :</td>
+                            <td>
+                                <input type="text" id="name" name="name" required>
+                                <div id="nameError" style="color:red;"></div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Age :</td>
+                            <td>
+                                <input type="text" id="age" name="age" required>
+                                <div id="ageError" style="color:red;"></div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Blood Group :</td>
+                            <td>
+                                <select id="bloodGroup" name="bloodGroup" required>
+                                    <option value="">Select Blood Group</option>
+                                    <option value="A+">A+</option>
+                                    <option value="A-">A-</option>
+                                    <option value="B+">B+</option>
+                                    <option value="B-">B-</option>
+                                    <option value="AB+">AB+</option>
+                                    <option value="AB-">AB-</option>
+                                    <option value="O+">O+</option>
+                                    <option value="O-">O-</option>
+                                </select>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Gender :</td>
+                            <td>
+                                <input type="radio" name="gender" value="Male" required> Male
+                                <input type="radio" name="gender" value="Female"> Female
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Date of Birth :</td>
+                            <td>
+                                <input type="date" id="dob" name="dob" required>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Email :</td>
+                            <td>
+                                <input type="text" id="email" name="email" required>
+                                <div id="emailError" style="color:red;"></div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Password :</td>
+                            <td>
+                                <input type="password" id="password" name="password" required>
+                                <div id="passwordError" style="color:red;"></div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Confirm Password :</td>
+                            <td>
+                                <input type="password" id="confirmPassword" name="confirmPassword" required>
+                                <div id="confirmError" style="color:red;"></div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Phone Number :</td>
+                            <td>
+                                <input type="text" id="phone" name="phone" required>
+                                <div id="phoneError" style="color:red;"></div>
+                            </td>
+                        </tr>
+                    </table>
+                    <input type="submit" value="Register"><div id="successMessage" style="color:green;"></div>
+                </form>
+                <form action="../../Login/View/login.php">
+                    <input type="submit" value="Login">
+                </form>
+        </fieldset>
+    </body>
+</html>
