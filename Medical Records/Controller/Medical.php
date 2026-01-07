865fd431187f7a6ab022a3af96cb9fd2b4518644
Rizvi Rahman Sefat <sefathasann@gmail.com>
2026-01-08 05:58:11 +0600

    Delete Medical Records directory

--- Medical Records/Controller/Medical.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode(['success'=>false,'message'=>'This endpoint is deprecated. Use Medical Records/View/patientMedicalRecords.php for AJAX.']);
-?>
