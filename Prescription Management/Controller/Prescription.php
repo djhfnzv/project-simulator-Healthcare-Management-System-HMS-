0a7d655013bfc69df5bf8e92e6963950eeb3d266
Rizvi Rahman Sefat <sefathasann@gmail.com>
2026-01-08 05:58:38 +0600

    Delete Prescription Management directory

--- Prescription Management/Controller/Prescription.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode(['success'=>false,'message'=>'This endpoint is deprecated. Use Prescription Management/View/prescriptionManagement.php for AJAX.']);
-?>
